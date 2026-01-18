e6b5f0ae863ff6b8ef330af049d896372eb1574d
johnwrk4114 <johnwrk4114@gmail.com>
2026-01-19 04:48:33 +0800

    Update tilt.js

--- tilt.js
+++ tilt.js
@@ -1,10 +1,8 @@
 const aboutWrapper = document.getElementById('aboutWrapper');
-const aboutImage = document.getElementById('aboutImage');
 
 document.addEventListener('mousemove', (e) => {
   const x = (e.clientX / window.innerWidth - 0.5) * 10; // 左右倾斜
   const y = (e.clientY / window.innerHeight - 0.5) * 10; // 上下倾斜
 
   aboutWrapper.style.transform = `rotateY(${x}deg) rotateX(${-y}deg)`;
-  aboutImage.style.transform = `rotateY(${x}deg) rotateX(${-y}deg)`;
 });
