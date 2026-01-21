cb5c8c80165b41dbe4da9540986184f1d42a97fd
johnwrk4114 <johnwrk4114@gmail.com>
2026-01-22 01:15:48 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,6 +1,1 @@
-document.querySelector(".logout-btn").addEventListener("click", () => {
-  const confirmLogout = confirm("Are you sure you want to logout?");
-  if (confirmLogout) {
-    window.location.href = "index.html";
-  }
-});
+console.log("Trading Wallet Dashboard Loaded");
