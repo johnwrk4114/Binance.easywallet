12875344c6c1a7e4be32da46ed556a75e75678d1
johnwrk4114 <johnwrk4114@gmail.com>
2026-01-23 17:42:52 +0800

    Delete tilt.js

--- tilt.js
+++
@@ -1,8 +0,0 @@
-const aboutWrapper = document.getElementById('aboutWrapper');
-
-document.addEventListener('mousemove', (e) => {
-  const x = (e.clientX / window.innerWidth - 0.5) * 10; // 左右倾斜
-  const y = (e.clientY / window.innerHeight - 0.5) * 10; // 上下倾斜
-
-  aboutWrapper.style.transform = `rotateY(${x}deg) rotateX(${-y}deg)`;
-});
