d517796acbfbbde8cdc4cec23fca97a1023173a7
johnwrk4114 <johnwrk4114@gmail.com>
2026-01-28 02:52:54 +0800

    Update utils.js

--- common/js/utils.js
+++ common/js/utils.js
@@ -1,1 +1,9 @@
-
+// 只封装2-3个每个页面都可能用到的函数
+window.Utils = {
+    showToast: function(message, type = 'info') {
+        // ... 你现有的showToast代码，确保在所有页面都能弹出通知 ...
+    },
+    formatCurrency: function(amount) {
+        return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(amount);
+    }
+};
